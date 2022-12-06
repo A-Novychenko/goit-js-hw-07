c093b2bbaee3fee84b490b943d446a4df85eee35
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-06 16:01:58 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,9 +3,7 @@ import {galleryItems} from "./gallery-items.js";
 // Завдання 1 - галерея зображень
 // Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
 
-// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
-
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
 // Реалізація делегування на div.gallery і отримання url великого зображення.
 // Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
 // Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
@@ -57,8 +55,21 @@ function onGalleryContainerClick(e) {
     return;
   }
 
+  window.addEventListener("keydown", onCloseModalKeypress);
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600">`
+  );
+
   e.preventDefault();
-  console.log("По каких элементах нужно отловить клики?");
-  console.log(e.target.src);
-  console.log((e.target.src = e.target.dataset.source));
+  instance.show();
+}
+
+function onCloseModalKeypress(e) {
+  if (e.code === "Escape") {
+    window.removeEventListener("keydown", onCloseModalKeypress);
+    console.log("esc");
+    // basicLightbox.close();
+  }
+  console.log("no esc");
 }
