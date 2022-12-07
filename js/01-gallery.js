19683172a7aa283ef0f8271a94bd57de1fef744f
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-07 15:32:08 +0200

    completes task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -13,17 +13,17 @@ function createGalleryCardsMarkup(galleryItems) {
   return galleryItems
     .map(({preview, original, description}) => {
       return `<div class="gallery__item">
-<a class="gallery__link" href="${original}">
-<img
-class="gallery__image"
-src="${preview}"
-data-source="${original}"
-alt="${description}"
-/>
-</a>
-</div>`;
+                <a class="gallery__link" href="${original}">
+                  <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                  />
+                </a>
+              </div>`;
     })
-    .join();
+    .join("");
 }
 
 function onGalleryContainerClick(e) {
